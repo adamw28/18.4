01e207212ce7c73aa3d8b95b75afc453f3f180bb
Adam Więcek <adamw28@gmail.com>
2018-02-04 22:02:50 +0100

    Update project

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+plugins = [new HtmlWebpackPlugin({}),
+    new webpack.optimize.UglifyJsPlugin()
+]
+let env = 'development';
+module.exports = {
+    entry: (env !== 'production' ? [
+        'react-hot-loader/patch',
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+    ] : []).concat(['./client/index.js']),
+    output: {
+        filename: './bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                loader: "babel-loader",
+                query: { compact: false }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: plugins
+};
